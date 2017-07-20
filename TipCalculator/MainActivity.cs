737a3905aa4c64a13c199d081f8863e267f2af21
Jon Bachelor <jon_bachelor@me.com>
2017-07-20 07:02:45 -0700

    Completion of exercise 2:  Basic UI with no functionality.

--- TipCalculator/MainActivity.cs
+++ TipCalculator/MainActivity.cs
@@ -15,16 +15,8 @@ namespace TipCalculator
 
         protected override void OnCreate(Bundle bundle)
         {
-            base.OnCreate(bundle);
-
-            // Set our view from the "main" layout resource
+            base.OnCreate(bundle);        
             SetContentView(Resource.Layout.Main);
-
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
-
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
         }
     }
 }
